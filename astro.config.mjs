92cd33f647b0e84241e4fe2dfad7f305d9a1dfda
xfeng01 <xsfeng@g.ucla.edu>
2024-06-15 20:08:29 -0700

    support math

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pride7.github.io",
   base: "/",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
