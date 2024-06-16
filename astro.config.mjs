38069a852bc0d8c78c63bcca5ff4f86cd6800785
xfeng01 <xsfeng@g.ucla.edu>
2024-06-15 20:38:45 -0700

    add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pride7.github.io",
@@ -10,4 +12,10 @@ export default defineConfig({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+  ],
 });
