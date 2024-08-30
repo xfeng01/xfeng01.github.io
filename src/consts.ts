15c95fd09ec1f9493f09d74e6ba7b52878365087
xfeng01 <xsfeng@g.ucla.edu>
2024-08-30 13:19:53 -0700

    add: global configure file and BaseHead Component.

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const site = {
+  title: "Xinsong Feng | 冯欣淞 - Homepage", // required
+  favicon: "/favicon.png", // required
+  author: "pride7", // required
+  avatar: "/megumi.jpg", // required
+};
