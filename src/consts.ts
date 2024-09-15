7bca26bf8c2b38cf5743083deac31187b64b51d6
xfeng01 <xsfeng@g.ucla.edu>
2024-09-15 02:47:48 -0700

    refactor: by configure

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,36 @@
 export const site = {
   title: "Xinsong Feng | 冯欣淞 - Homepage", // required
   favicon: "/favicon.png", // required
+  description: "Msc in ECE",
   author: "Xinsong Feng", // required
+  author_CN: "冯欣淞", // required
   avatar: "/xsfeng.jpg", // required
 };
+
+export const info = {
+  location: {
+    icon: "fas fa-map-marker-alt",
+    color: "text-blue-500",
+    name: "Los Angeles",
+  },
+  social: [
+  {
+    icon: "fas fa-envelope",
+    color: "text-green-500",
+    name: "Email",
+    link: "mailto:xsfeng@g.ucla.com",
+  },
+  {
+    icon: "fab fa-github",
+    color: "text-gray-700",
+    name: "Github",
+    link: "https://github.com/xfeng01",
+  },
+  {
+    icon: "fas fa-graduation-cap",
+    color: "text-red-500",
+    name: "Google Scholar",
+    link: "https://scholar.google.com/citations?hl=en&user=664D7CoAAAAJ",
+  },
+]
+}
