5b7a146422019a23466b66e0c382641a01c63c8c
xfeng01 <xsfeng@g.ucla.edu>
2025-05-01 05:17:07 +0800

    fix: bugs

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,6 @@
 import { z, defineCollection } from "astro:content";
 
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
 const posts = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
